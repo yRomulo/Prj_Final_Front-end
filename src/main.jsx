40ecf201c29a4f828f8d93a6d4c22f2cd3d43ca4
yRomulo <116182111+yRomulo@users.noreply.github.com>
2024-10-02 19:06:58 -0300

    LoginPageAdd

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Details } from "./pages/Details";
+import { Home } from "./pages/Home";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details/>
+      <Home/>
     </ThemeProvider>
   </React.StrictMode>
 );
